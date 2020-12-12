6346847e4699df4d7eb5d1ce462885b635ac8b13
Nabil Arbouz <nabilarbouz33@gmail.com>
2020-12-12 02:22:41 -0800

    fixed commenting bug

--- app/src/main/java/edu/neu/madcourse/recoio/ui/review/ReviewFragment.java
+++ app/src/main/java/edu/neu/madcourse/recoio/ui/review/ReviewFragment.java
@@ -2,6 +2,7 @@ package edu.neu.madcourse.recoio.ui.review;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -39,6 +41,8 @@ import java.util.Date;
 
 import edu.neu.madcourse.recoio.Comment;
 import edu.neu.madcourse.recoio.R;
+
+import static androidx.core.content.ContextCompat.getSystemService;
 
 // TODO: exit out of review once posted
 public class ReviewFragment extends Fragment {
@@ -199,6 +203,9 @@ public class ReviewFragment extends Fragment {
                     DatabaseReference newCommentReference = reviews.child(reviewUID)
                             .child("comments").child(String.valueOf(commentUID.getTime()));
                     newCommentReference.setValue(newComment);
+                    commentEditText.setText("");
+                    InputMethodManager imm = (InputMethodManager) requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(commentEditText.getWindowToken(), 0);
                 }
 
                 @Override
